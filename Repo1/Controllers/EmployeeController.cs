9f306eb316ea0dfdcc6ee652501efe1f686f4ec6
Nitish Sannanse <nitishss7822@gmail.com>
2025-05-22 16:26:57 +0530

    to test conflict from local

--- Repo1/Controllers/EmployeeController.cs
+++ Repo1/Controllers/EmployeeController.cs
@@ -112,7 +112,7 @@ namespace Repo1.Controllers
             var stud =_studentRepo.getStudentById(id);
             Console.Write("first change from local");
             Console.Write("second change from Feature new branch");
-            Console.Write("Third feature from Feature branch");
+            Console.Write("Third feature from Feature branch to test conflicts");
             return Ok(stud);
         }
 
